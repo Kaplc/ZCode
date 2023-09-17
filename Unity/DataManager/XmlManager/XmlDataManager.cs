d64daa6d1289b4244052dcd5b1c0dcef5c67716e
Kaplc <755536175@qq.com>
2023-09-17 10:41:35 +0800

    更新basePanel

---
+++ Unity/DataManager/XmlManager/XmlDataManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Xml.Serialization;
+using UnityEngine;
+
+public class XmlDataManager
+{
+    public static XmlDataManager Instance { get; } = new XmlDataManager();
+
+    private XmlDataManager()
+    {
+    }
+
+    public void Save(object data, string fileName)
+    {
+        string path = Application.persistentDataPath + "/" + fileName + ".xml";
+
+        using (StreamWriter file = new StreamWriter(path))
+        {
+            XmlSerializer ser = new XmlSerializer(data.GetType());
+            ser.Serialize(file, data);
+        }
+    }
+
+    public object Load(Type type, string fileName)
+    {
+        string path = Application.persistentDataPath + "/" + fileName + ".xml";
+
+        if (!File.Exists(path))
+        {
+            path = Application.streamingAssetsPath + "/" + fileName + ".xml";
+            if (!File.Exists(path))
+            {
+                // 两个路径都没有则创建空对象
+                return Activator.CreateInstance(type);
+            }
+        }
+
+        using (StreamReader file = new StreamReader(path))
+        {
+            XmlSerializer serializer = new XmlSerializer(type);
+            return serializer.Deserialize(file);
+        }
+    }
+}
